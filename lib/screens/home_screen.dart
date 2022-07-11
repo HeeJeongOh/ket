f97a49407fc4789c1db30c92b4cc82b0f5c7f610
gobi <ohhj1999@gmail.com>
2022-07-11 23:59:31 +0900

    add setting and list

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ket/constants.dart';
+import 'package:ket/screens/setting_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   List<Map<String, String>> categoryCardData = [
@@ -22,52 +23,96 @@ class HomeScreen extends StatelessWidget {
                 onPressed: () {},
                 icon: SvgPicture.asset("assets/icons/search.svg")),
             IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SettingScreen()),
+                  );
+                },
                 icon: SvgPicture.asset("assets/icons/more-vertical.svg")),
           ],
         )
       ]),
-      body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
+      body: Center(
+          child: Column(children: [
+        SizedBox(
+          height: 10,
+        ),
+        SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Container(
+                    width: 80 * (categoryCardData.length + 1),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: List.generate(
+                        categoryCardData.length,
+                        (index) => CategoryMediumCard(
+                            name: categoryCardData[index]['name'],
+                            bcolor: categoryCardData[index]['color']),
+                      ),
+                    )))),
+        SingleChildScrollView(
+          scrollDirection: Axis.vertical,
           child: Padding(
-              padding: EdgeInsets.all(10),
-              child: Container(
-                  width: 80 * (categoryCardData.length + 1),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: List.generate(
-                      categoryCardData.length,
-                      (index) => CategoryMediumCard(
-                          name: categoryCardData[index]['name']),
-                    ),
-                  )))),
+              padding: EdgeInsets.all(20),
+              child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: ((context, index) => bucketCard()))),
+        )
+      ])),
       // 전체 배경
     );
   }
 }
 
+class bucketCard extends StatelessWidget {
+  const bucketCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(20),
+        child: Expanded(
+            child: Row(
+          children: [
+            Text(
+              '카테고리 ',
+              style: kSubtitleTextStyle,
+            ),
+            SvgPicture.asset('assets/icons/user.png')
+          ],
+        )));
+  }
+}
+
 class CategoryMediumCard extends StatelessWidget {
-  const CategoryMediumCard({Key? key, required this.name}) : super(key: key);
+  const CategoryMediumCard({Key? key, required this.name, required this.bcolor})
+      : super(key: key);
 
   final String? name;
+  final String? bcolor;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-        borderRadius: BorderRadius.all(Radius.circular(6)),
+        borderRadius: const BorderRadius.all(Radius.circular(6)),
         onTap: () {},
         child: Container(
           width: 80,
           height: 50,
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
           child: Text(
             name!,
             textAlign: TextAlign.center,
-            style: TextStyle(
+            style: const TextStyle(
                 color: kTextColor, fontSize: 15, fontWeight: FontWeight.bold),
           ),
-          decoration: BoxDecoration(
-            color: kPrimaryLightColor,
+          decoration: const BoxDecoration(
+            color: kBlueColor,
             borderRadius: BorderRadius.all(Radius.circular(10)),
           ),
         ));
